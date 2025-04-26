65b3dd039435665fe4d2a806469f7ea5a124bee4
mahlet-16 <mahletamenu16@gmail.com>
2025-04-26 18:35:45 +0300

    build my portfolio using react

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import './Footer.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="mb-0">
+      Copyright &copy; {new Date().getFullYear()}
+      <a href="https://github.com/mahlet-16" className="footer-link"> Mahlet Amenu</a>
+    </p>
+    <div className="social-links text-center">
+      <a href="https://www.linkedin.com/in/mihret-tsegaye-nega/" className="link" aria-label="LinkedIn">
+        <i className="fab fa-linkedin" />
+      </a>
+      <a href="https://t.me/maltb" className="link" aria-label="Telegram">
+        <i className="fab fa-telegram" />
+      </a>
+      <a href="mahletamenu16@gmail.com" className="link" aria-label="Email">
+        <i className="fas fa-envelope" />
+      </a>
+      <a href="https://github.com/mahlet-16" className="link" aria-label="GitHub">
+        <i className="fab fa-github" />
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/portfolio.js
@@ -0,0 +1,74 @@
+const header = {
+  homepage: 'https://mihrettsegaye.github.io/portfolio', // Update with actual portfolio link if available
+  title: 'MA',
+}
+
+const about = {
+  name: 'Mahlet Amenu',
+  role: 'Back-End Developer ',
+  description:
+    'A passionate and dedicated Computer Science student with a strong focus on backend development using Django. Experienced in building scalable web applications, designing RESTful APIs, and managing databases. Passionate about writing clean, efficient code and continuously improving backend performance. Eager to apply problem-solving skills and technical expertise to real-world projects',
+  resume: 'https://docs.google.com/document/d/1nnWdaYazKrKokAbxq78-W5NWYQEuYUGZ5xRxugdZBks/edit?tab=t.0', // Update with actual resume link if available
+  social: {
+    linkedin: 'https://www.linkedin.com/in/mihret-tsegaye-nega/', // Update with actual LinkedIn profile link
+    github: 'https://github.com/mahlet-16', // Update with actual GitHub profile link
+  },
+}
+
+const projects = [
+  {
+    name: 'Cleaning Website',
+    description:
+      ' A class project built for a local cleaning service company. It is a web-based application designed to manage service bookings, client information, and staff schedules efficiently.',
+    stack: ['Bootstrap', 'php', 'MySQL'],
+    sourceCode: 'https://github.com/mahlet-16/Cleanii', // Update with actual GitHub repo link
+    livePreview: 'https://mahlet-16.github.io/Cleanii/', // Update with actual live preview link
+  },
+  {
+    name: 'Gibi Website',
+    description:
+    ' A class project built for cpu college students which participate in this club. It is a web-based application designed to manage register new students, detail information, and program schedules efficiently.',
+    stack: ['React'],
+    sourceCode: 'https://github.com/mahlet-16/Gibi', // Placeholder link
+    livePreview: ' https://mahlet-16.github.io/Gibi/Gibi-Website/', // Placeholder link
+  },
+  {
+    name: ' Team Portfolio',
+    description:
+      'This project is a dynamic portfolio website built for a tech team of six members. Developed using Bootstrap for a responsive and modern front-end, PHP for server-side logic, and WAMP Server (Windows, Apache, MySQL, PHP) for local development and database management.',
+    stack: [ 'Bootstrap', 'php', 'MySQL'],
+    sourceCode: 'https://github.com/mahlet-16/Helix-s-Portfolio', // Placeholder link
+    livePreview: ' https://mahlet-16.github.io/Helix-s-Portfolio/', // Placeholder link
+  },
+  {
+    name: 'EventManagement API',
+    description:
+      'This project is a backend-only Event Management system built with Django. It provides a RESTful API to manage events, users, and registrations. Designed for flexibility and scalability, the API allows easy integration with any frontend (mobile apps, web apps, admin panels, etc.).',
+    stack: [ 'Django'],
+    sourceCode: 'https://github.com/mahlet-16/EventManagement_api', // Placeholder link
+  },
+  {
+    name: 'API for online market',
+    description:
+      'TThis project is a backend-only system built with Django to manage the sale of skincare products. It exposes a RESTful API that handles product listings, customer orders, inventory management, and user authentication. The backend is designed to easily integrate with any frontend website, mobile app, or admin dashboard.',
+    stack: [ 'Django'],
+    sourceCode: 'https://github.com/mahlet-16/project_2/tree/main', // Placeholder link
+  },
+]
+
+const skills = [
+  'Django',
+  'Python',
+  'php',
+  'REACT',
+  'Java',
+  'bootstrap',
+  'c++',
+  
+]
+
+const contact = {
+  email: 'mahletamenu16@gmail.com',
+}
+
+export { header, about, projects, skills, contact}
